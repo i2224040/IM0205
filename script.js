c2d814a829bcd50b0adbd82475f72aab21f124e1
i2224040 <130639800+i2224040@users.noreply.github.com>
2024-05-24 01:38:28 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('photoCanvas');
     const overlay = document.getElementById('overlay');
     let context = canvas.getContext('2d');
-    let transparency = 0;
     let gameStarted = false;
-    let holdInterval;
 
     // カメラのストリームを取得（内カメラ、40:30の比率）
     navigator.mediaDevices.getUserMedia({
@@ -28,53 +26,29 @@ document.addEventListener('DOMContentLoaded', () => {
         context.drawImage(video, 0, 0, canvas.width, canvas.height);
         overlay.style.backgroundImage = `url(${canvas.toDataURL('image/png')})`;
         video.style.display = 'none'; // ビデオフィードを非表示にする
-        overlay.style.opacity = 0; // 透明度を初期化
-        transparency = 0; // 透明度を初期化
     }
 
-    // 透明度を増加させる関数
-    function increaseTransparency() {
-        if (transparency < 100) {
-            transparency++;
-            overlay.style.opacity = transparency / 100;
-            console.log(`Transparency increased to: ${transparency}`);
-        }
+    // スライダーで透明度を変更する関数
+    function changeTransparency(value) {
+        overlay.style.opacity = value / 100;
     }
 
-    // 背景色をランダムに変更する関数
-    function changeBackgroundColor() {
-        const randomColor = Math.floor(Math.random()*16777215).toString(16);
-        document.body.style.backgroundColor = `#${randomColor}`;
-    }
+    // スライダーのDOM要素を取得
+    const slider = document.getElementById('transparencySlider');
 
-    // 長押しイベントリスナー
+    // スライダーの値が変更されたときのイベントリスナーを追加
+    slider.addEventListener('input', () => {
+        const value = slider.value;
+        changeTransparency(value);
+    });
+
+    // 画面をタップしたときのイベントリスナーを追加
     document.addEventListener('mousedown', () => {
         if (!gameStarted) {
             takeScreenshot();
-            changeBackgroundColor();
             gameStarted = true;
-        } else {
-            holdInterval = setInterval(increaseTransparency, 100);
         }
-    });
-
-    document.addEventListener('mouseup', () => {
-        clearInterval(holdInterval);
-    });
-
-    document.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-        if (!gameStarted) {
-            takeScreenshot();
-            changeBackgroundColor();
-            gameStarted = true;
-        } else {
-            holdInterval = setInterval(increaseTransparency, 100);
-        }
-    });
-
-    document.addEventListener('touchend', (e) => {
-        e.preventDefault();
-        clearInterval(holdInterval);
     });
 });
+
+
